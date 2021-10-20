fed2d1ed254f6ffb2715fe6c39e00dad1387e8ca
Chris Wininger <cwininger@sonatype.com>
2021-10-20 16:40:01 -0400

    initial commit

---
+++ src/main/java/com/example/allthethings/AllthethingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.allthethings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllthethingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllthethingsApplication.class, args);
+	}
+
+}
